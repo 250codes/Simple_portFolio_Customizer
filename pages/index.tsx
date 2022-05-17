bc3de26e6c3ec9b640c49b3a60a1fb0cb328ec65
nkusikevin <nkusikvnhart@gmail.com>
2022-05-17 21:24:08 +0200

    adding terminal

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,37 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
 	return (
-		<div className='h-screen bg-black bg-opacity-60 backdrop-filter backdrop-blur'></div>
+		<div className='h-screen relative '>
+			<div className='absolute grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 w-full justify-center p-3 top-1/3 h-96'>
+				<div className='card'>
+					<div className='fakeMenu'>
+						<div className='fakeButtons fakeClose'></div>
+						<div className='fakeButtons fakeMinimize'></div>
+						<div className='fakeButtons fakeZoom'></div>
+					</div>
+					<div className='fakeScreen'>
+						<p className='line1'>
+							&#91;&nbsp;&ldquo;I'm a web developer.&rdquo;,
+							<span className='cursor1'>_</span>
+						</p>
+						<p className='line2'>
+							&nbsp;&nbsp;&ldquo;I'm a web designer.&rdquo;,
+							<span className='cursor2'>_</span>
+						</p>
+						<p className='line3'>
+							&nbsp;&nbsp;&ldquo;Let's work together!&rdquo;&nbsp;&#93;
+							<span className='cursor3'>_</span>
+						</p>
+						<p className='line4'>
+							{`>`}
+							<span className='cursor4'>{`_`}</span>
+						</p>
+					</div>
+				</div>
+
+				<div className='card'>09</div>
+			</div>
+		</div>
 	);
 };
 
